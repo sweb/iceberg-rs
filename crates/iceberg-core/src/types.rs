03b041447884faca89a404ab6d5bea019fef3f84
Florian Müller <florian@tomueller.de>
2026-04-07 17:01:33 +0200

    add Transform custom deserialization

--- crates/iceberg-core/src/types.rs
+++ crates/iceberg-core/src/types.rs
@@ -1,3 +1,7 @@
+use serde::de::{self, Visitor};
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
 #[derive(Debug, PartialEq)]
 pub enum PrimitiveType {
     Boolean,
@@ -5,7 +9,7 @@ pub enum PrimitiveType {
     Long,
     Float,
     Double,
-    Decimal {precision: u32, scale: u32 },
+    Decimal { precision: u32, scale: u32 },
     Date,
     Time,
     Timestamp,
@@ -35,8 +39,7 @@ pub struct NestedField {
     pub name: String,
     pub required: bool,
     pub field_type: Box<Type>,
-    pub doc: Option<String>
-    // TODO: default value
+    pub doc: Option<String>, // TODO: default value
 }
 
 #[derive(Debug, PartialEq)]
@@ -61,6 +64,82 @@ pub struct TableSchema {
     pub fields: StructType,
 }
 
+#[derive(Debug, PartialEq, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum Transform {
+    Identity,
+    Bucket(u32),
+    Truncate(u32),
+    Year,
+    Month,
+    Day,
+    Hour,
+    Void,
+}
+
+// Implementation based on https://serde.rs/impl-deserialize.html
+struct TransformVisitor;
+
+impl<'de> Visitor<'de> for TransformVisitor {
+    type Value = Transform;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str(
+            "a supported Transform type (identity, bucket, truncate, year, month, day, hour, void)",
+        )
+    }
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        match value {
+            "identity" => Ok(Transform::Identity),
+            "year" => Ok(Transform::Year),
+            "month" => Ok(Transform::Month),
+            "day" => Ok(Transform::Day),
+            "hour" => Ok(Transform::Hour),
+            "void" => Ok(Transform::Void),
+            s if s.starts_with("bucket[") && s.ends_with("]") => {
+                let bucket_size = value[7..s.len() - 1]
+                    .parse::<u32>()
+                    .map_err(|e| E::custom(format!("Invalid bucket width: {}", e)))?;
+                Ok(Transform::Bucket(bucket_size))
+            }
+            s if s.starts_with("truncate[") && s.ends_with("]") => {
+                let width = value[9..s.len() - 1]
+                    .parse::<u32>()
+                    .map_err(|e| E::custom(format!("Invalid truncate width: {}", e)))?;
+                Ok(Transform::Truncate(width))
+            }
+            _ => Err(E::custom(format!("unknown transform: {}", value))),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for Transform {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(TransformVisitor)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct PartitionField {
+    source_id: u32,
+    field_id: u32,
+    name: String,
+    transform: Transform,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct PartitionSpec {
+    spec_id: u32,
+    fields: Vec<PartitionField>,
+}
 
 #[cfg(test)]
 mod tests {
@@ -68,6 +147,65 @@ mod tests {
 
     #[test]
     fn use_primitive_type() {
-        assert_eq!(Type::Primitive(PrimitiveType::Boolean), Type::Primitive(PrimitiveType::Boolean));
+        assert_eq!(
+            Type::Primitive(PrimitiveType::Boolean),
+            Type::Primitive(PrimitiveType::Boolean)
+        );
+    }
+
+    #[test]
+    fn deserialize_transform_identity() {
+        let raw = r#""identity""#;
+        let transform: Transform = serde_json::from_str(raw).unwrap();
+        assert_eq!(transform, Transform::Identity);
+    }
+
+    #[test]
+    fn deserialize_transform_year() {
+        let raw = r#""year""#;
+        let transform: Transform = serde_json::from_str(raw).unwrap();
+        assert_eq!(transform, Transform::Year);
+    }
+
+    #[test]
+    fn deserialize_transform_month() {
+        let raw = r#""month""#;
+        let transform: Transform = serde_json::from_str(raw).unwrap();
+        assert_eq!(transform, Transform::Month);
+    }
+
+    #[test]
+    fn deserialize_transform_day() {
+        let raw = r#""day""#;
+        let transform: Transform = serde_json::from_str(raw).unwrap();
+        assert_eq!(transform, Transform::Day);
+    }
+
+    #[test]
+    fn deserialize_transform_hour() {
+        let raw = r#""hour""#;
+        let transform: Transform = serde_json::from_str(raw).unwrap();
+        assert_eq!(transform, Transform::Hour);
+    }
+
+    #[test]
+    fn deserialize_transform_void() {
+        let raw = r#""void""#;
+        let transform: Transform = serde_json::from_str(raw).unwrap();
+        assert_eq!(transform, Transform::Void);
+    }
+
+    #[test]
+    fn deserialize_transform_bucket() {
+        let raw = r#""bucket[16]""#;
+        let transform: Transform = serde_json::from_str(raw).unwrap();
+        assert_eq!(transform, Transform::Bucket(16));
+    }
+
+    #[test]
+    fn deserialize_transform_truncate(){
+        let raw = r#""truncate[20]""#;
+        let transform: Transform = serde_json::from_str(raw).unwrap();
+        assert_eq!(transform, Transform::Truncate(20));
     }
 }
